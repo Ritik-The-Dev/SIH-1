5607334877d4ad552783741d8835f1197c7a25c6
Ritik-the-dev <joshiritik865@gmail.com>
2025-09-14 14:36:51 +0530

    Updated Frontend Build

--- sih-frontend/src/components/loginWithGoogle.tsx
+++ sih-frontend/src/components/loginWithGoogle.tsx
@@ -1,26 +1,36 @@
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import { useLoginWithGoogle } from "../services/hooks";
 import { useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { themeState } from "../store/theme";
 
 export default function GoogleLoginFunction() {
+    const darkMode = useRecoilValue(themeState);
     const navigate = useNavigate();
     const { mutateAsync: userLogin } = useLoginWithGoogle();
 
     return (
         <GoogleOAuthProvider clientId={import.meta.env.VITE_ENV_GOOGLE_CLIENT_ID}>
-            <GoogleLogin
-                onSuccess={credentialResponse => {
-                    userLogin({ idToken: credentialResponse.credential })
-                        .then(data => {
-                            if (data && data?.success) {
-                                navigate('/')
-                            }
-                        });
-                }}
-                onError={() => {
-                    console.log("Login Failed");
-                }}
-            />
+            <div className="w-full flex justify-center">
+                <GoogleLogin
+                    onSuccess={(credentialResponse) => {
+                        userLogin({ idToken: credentialResponse.credential })
+                            .then(data => {
+                                if (data && data?.success) {
+                                    navigate("/");
+                                }
+                            });
+                    }}
+                    onError={() => {
+                        console.log("Login Failed");
+                    }}
+                    theme={darkMode ? "filled_black" : "outline"} 
+                    size="large"
+                    width="100%"   
+                    shape="rectangular"
+                    text="continue_with"
+                />
+            </div>
         </GoogleOAuthProvider>
-    )
+    );
 }
